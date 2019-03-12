e618b234ebf9423704e7616911be846159fd4552
Gabriel Yshay <ggyshay@gmail.com>
2019-03-12 09:51:47 -0300

    Updates

---
+++ precache-manifest.a005dfd2b1e6e7e3bcfa2156e4687a63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae828b4d6c8f1c1832c073eb56ae8a0",
+    "url": "/sintese/static/media/wave0001-0128.3ae828b4.webm"
+  },
+  {
+    "revision": "ad5e64e30795f77f6d981db895316b30",
+    "url": "/sintese/static/media/timbres.ad5e64e3.png"
+  },
+  {
+    "revision": "88e648711b867598b6453e70b65fa425",
+    "url": "/sintese/static/media/room-waves.88e64871.mp4"
+  },
+  {
+    "revision": "6d6d434287ac8b1d8dac1ee2ca86b97e",
+    "url": "/sintese/static/media/fm.6d6d4342.mp4"
+  },
+  {
+    "revision": "f33d2d4178524f597d6596a347333d43",
+    "url": "/sintese/static/media/fm-schema.f33d2d41.png"
+  },
+  {
+    "revision": "76f68d94292c1c03dd75",
+    "url": "/sintese/static/js/runtime~main.76f68d94.js"
+  },
+  {
+    "revision": "1668e49a2c50a022d741",
+    "url": "/sintese/static/js/main.1668e49a.chunk.js"
+  },
+  {
+    "revision": "5114e3664050b4abf994",
+    "url": "/sintese/static/js/1.5114e366.chunk.js"
+  },
+  {
+    "revision": "1668e49a2c50a022d741",
+    "url": "/sintese/static/css/main.84d986db.chunk.css"
+  },
+  {
+    "revision": "4d7005b51d8abcaaa28a103c4f3ba86d",
+    "url": "/sintese/index.html"
+  }
+];
