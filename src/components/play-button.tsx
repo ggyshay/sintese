f11906d4930ae5d4cbe3ec09d907c6e8648efeb0
yshay <yshay@cuidas.com.br>
2020-08-30 15:40:12 -0300

    change play color

--- src/components/play-button.tsx
+++ src/components/play-button.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import "./play-button.scss"
+import React from "react";
+import "./play-button.css";
 
 export interface PlayButtonProps {
-    playing: boolean;
-    onClick: () => void;
+  playing: boolean;
+  onClick: () => void;
 }
 
-export class PlayButton extends React.PureComponent<PlayButtonProps>{
-    render() {
-        return (
-            <div className={`play-button ${this.props.playing && 'paused'}`} onClick={this.props.onClick}/>
-        )
-    }
+export class PlayButton extends React.PureComponent<PlayButtonProps> {
+  render() {
+    return (
+      <div
+        className={`play-button ${this.props.playing && "paused"}`}
+        onClick={this.props.onClick}
+      />
+    );
+  }
 }
